ccd5f54f69a554f92d61a1e6ba18e95e19073602
NThakur4 <nthakur4@slb.com>
2021-05-21 11:36:54 -0500

    update doc to reflect correct page size

--- src/main/java/org/opengroup/osdu/core/common/SwaggerDoc.java
+++ src/main/java/org/opengroup/osdu/core/common/SwaggerDoc.java
@@ -77,7 +77,7 @@ public final class SwaggerDoc {
     public static final String SORT_NOT_VALID_ORDER_OPTION = "Not a valid order option. It can only be either 'ASC' or 'DESC'";
     public static final String SORT_ORDER_VALIDATION_NOT_EMPTY_MSG = "'sort.order' can not be null or empty";
     public static final String SORT_FIELD_ORDER_SIZE_NOT_MATCH = "'sort.field' and 'sort.order' size do not match";
-    public static final String LIMIT_DESCRIPTION = "The maximum number of results to return from the given offset. If no limit is provided, then it will return 10 items. Max number of items which can be fetched by the query is 100. (If you wish to fetch large set of items, please use query_with_cursor API)";
+    public static final String LIMIT_DESCRIPTION = "The maximum number of results to return from the given offset. If no limit is provided, then it will return 10 items. Max number of items which can be fetched by the query is 1000. (If you wish to fetch large set of items, please use query_with_cursor API)";
     public static final String QUERY_DESCRIPTION = "The query string in Lucene query string syntax.";
     public static final String SORT_DESCRIPTION = "The fields and orders to return sorted results.";
     public static final String SORT_FIELD_DESCRIPTION = "The list of fields to sort the results.";
