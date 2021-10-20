0ad19c1805a4ee4ae4350b5b6dce314f9394998f
NThakur4 <nthakur4@slb.com>
2021-10-20 15:47:59 -0500

    add scope

--- src/main/java/org/opengroup/osdu/core/common/model/indexer/SchemaChangedMessages.java
+++ src/main/java/org/opengroup/osdu/core/common/model/indexer/SchemaChangedMessages.java
@@ -20,12 +20,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RequestScope
 public class SchemaChangedMessages {
 
     private String messageId;
