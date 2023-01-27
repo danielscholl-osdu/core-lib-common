308696d82962ee4b06c52106aa5f1f08185d1414
Alok Joshi <AJoshi19@slb.com>
2023-01-26 23:36:11 -0600

    fix bean creation

--- src/main/java/org/opengroup/osdu/core/common/feature/FeatureFlagConfig.java
+++ src/main/java/org/opengroup/osdu/core/common/feature/FeatureFlagConfig.java
@@ -1,48 +1,21 @@
 package org.opengroup.osdu.core.common.feature;
 
 import org.opengroup.osdu.core.common.logging.JaxRsDpsLog;
-import org.opengroup.osdu.core.common.model.http.DpsHeaders;
-import org.opengroup.osdu.core.common.partition.IPartitionFactory;
-import org.opengroup.osdu.core.common.partition.IPartitionProvider;
-import org.opengroup.osdu.core.common.partition.PartitionAPIConfig;
-import org.opengroup.osdu.core.common.partition.PartitionFactory;
-import org.opengroup.osdu.core.common.util.IServiceAccountJwtClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @ConditionalOnProperty(prefix = "featureFlag", name = "strategy")
 public class FeatureFlagConfig {
     @Autowired
-    private DpsHeaders headers;
-
-    @Autowired
     private JaxRsDpsLog logger;
-
-    IPartitionProvider partitionProvider;
-
-    private IServiceAccountJwtClient tokenService;
-
-    @Value("${PARTITION_API:not_used}")
-    private String partitionAPIEndpoint;
 
     @Bean
     @ConditionalOnProperty(prefix = "featureFlag", name = "strategy", havingValue = "dataPartition")
     public PartitionFeatureFlagImpl dataPartitionFeatureFlag() {
-        PartitionAPIConfig apiConfig = PartitionAPIConfig.builder()
-                .rootUrl(partitionAPIEndpoint)
-                .build();
-        IPartitionFactory iPartitionFactory = new PartitionFactory(apiConfig);
-        DpsHeaders partitionHeaders = DpsHeaders.createFromMap(headers.getHeaders());
-        partitionHeaders.put(DpsHeaders.AUTHORIZATION, tokenService.getIdToken(headers.getPartitionId()));
-        partitionProvider = iPartitionFactory.create(partitionHeaders);
-        return new PartitionFeatureFlagImpl(partitionProvider, logger, partitionHeaders);
+        return new PartitionFeatureFlagImpl(logger);
     }
 
     @Bean
--- src/main/java/org/opengroup/osdu/core/common/feature/PartitionFeatureFlagImpl.java
+++ src/main/java/org/opengroup/osdu/core/common/feature/PartitionFeatureFlagImpl.java
@@ -3,31 +3,51 @@ package org.opengroup.osdu.core.common.feature;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import lombok.RequiredArgsConstructor;
 import org.opengroup.osdu.core.common.logging.JaxRsDpsLog;
 import org.opengroup.osdu.core.common.model.http.AppException;
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
+import org.opengroup.osdu.core.common.partition.IPartitionFactory;
 import org.opengroup.osdu.core.common.partition.IPartitionProvider;
+import org.opengroup.osdu.core.common.partition.PartitionAPIConfig;
 import org.opengroup.osdu.core.common.partition.PartitionException;
+import org.opengroup.osdu.core.common.partition.PartitionFactory;
 import org.opengroup.osdu.core.common.partition.PartitionInfo;
+import org.opengroup.osdu.core.common.util.IServiceAccountJwtClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
-@RequiredArgsConstructor
 public class PartitionFeatureFlagImpl implements IFeatureFlag {
-    private final IPartitionProvider partitionProvider;
     private final JaxRsDpsLog logger;
-    private final DpsHeaders dpsHeaders;
+    @Autowired
+    private DpsHeaders headers;
+    @Autowired
+    private IServiceAccountJwtClient tokenService;
+
+    @Value("${PARTITION_API:not_used}")
+    private String partitionAPIEndpoint;
+
+    public PartitionFeatureFlagImpl(JaxRsDpsLog logger) {
+        this.logger = logger;
+    }
 
     @Override
     public boolean isFeatureEnabled(String featureName) {
+        PartitionAPIConfig apiConfig = PartitionAPIConfig.builder()
+                .rootUrl(partitionAPIEndpoint)
+                .build();
+        IPartitionFactory partitionFactory = new PartitionFactory(apiConfig);
+        DpsHeaders partitionHeaders = DpsHeaders.createFromMap(headers.getHeaders());
+        partitionHeaders.put(DpsHeaders.AUTHORIZATION, tokenService.getIdToken(headers.getPartitionId()));
+        IPartitionProvider partitionProvider = partitionFactory.create(partitionHeaders);
+
         if (partitionProvider == null)
             return false;
-
         try {
-            PartitionInfo partitionInfo = partitionProvider.get(dpsHeaders.getPartitionId());
+            PartitionInfo partitionInfo = partitionProvider.get(headers.getPartitionId());
             return getFeatureFlagStatus(partitionInfo, featureName);
         } catch (PartitionException e) {
-            this.logger.error(String.format("Error getting feature flag status for dataPartitionId: %s", dpsHeaders.getPartitionId()), e);
-            throw new AppException(e.getHttpResponse().getResponseCode(), String.format("Error getting feature flag value for property %s, partition %s ", featureName, dpsHeaders.getPartitionId()), e.getMessage(), e);
+            this.logger.error(String.format("Error getting feature flag status for dataPartitionId: %s", headers.getPartitionId()), e);
+            throw new AppException(e.getHttpResponse().getResponseCode(), String.format("Error getting feature flag value for property %s, partition %s ", featureName, headers.getPartitionId()), e.getMessage(), e);
         }
     }
 
