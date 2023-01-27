eb26bfbd9b7120e51bb19f05ca63499a4ca09001
Alok Joshi <AJoshi19@slb.com>
2023-01-27 11:57:16 -0600

    remove unused import

--- src/main/java/org/opengroup/osdu/core/common/feature/IFeatureFlag.java
+++ src/main/java/org/opengroup/osdu/core/common/feature/IFeatureFlag.java
@@ -1,7 +1,5 @@
 package org.opengroup.osdu.core.common.feature;
 
-import org.opengroup.osdu.core.common.partition.PartitionException;
-
 public interface IFeatureFlag {
     public boolean isFeatureEnabled(String featureName);
 }
